9b70d9316df5c71d1f0ffdf36d2053f654abcfcf
Rony Peterson V Conde <ronyconde@MacBook-Pro-de-Rony.local>
2015-05-04 11:04:00 -0400

    criação de  nova classe

---
+++ learningHowToGit/learningHowToGit/testeClasse.h
@@ -0,0 +1,13 @@
+//
+//  testeClasse.h
+//  learningHowToGit
+//
+//  Created by Rony Peterson V Conde on 04/05/15.
+//  Copyright (c) 2015 Carlos Soares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testeClasse : UITableViewCell
+
+@end
