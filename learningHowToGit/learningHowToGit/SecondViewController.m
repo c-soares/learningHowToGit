e097f348b11c2e185a54c025fb5526aa64770736
Carlos Soares <carlossoares@Carlos-Soares-Mac.local>
2015-05-04 10:27:44 -0400

    Editing size of tab bar control (4.7 inches) and adding Carlo's images and button function

--- learningHowToGit/learningHowToGit/SecondViewController.m
+++ learningHowToGit/learningHowToGit/SecondViewController.m
@@ -9,6 +9,8 @@
 #import "SecondViewController.h"
 
 @interface SecondViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *labelName;
+@property NSInteger num;
 
 @end
 
@@ -17,6 +19,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.num = 1;
+}
+- (IBAction)playName:(id)sender {
+    switch (_num) {
+        case 0:
+            self.labelName.text = @"Carlos"; self.num++;
+            break;
+        case 1:
+            self.labelName.text = @"Henrique"; self.num++;
+            break;
+        case 2:
+            self.labelName.text = @"Palheta"; self.num++;
+            break;
+        case 3:
+            self.labelName.text = @"Soares"; self.num = 0;
+            break;
+            
+        default:
+            break;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
